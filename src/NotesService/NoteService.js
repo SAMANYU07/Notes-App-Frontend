9111b0685ade8113f4c8623f82b2dc17de947373
SAMANYU07 <samanyu1003@gmail.com>
2024-09-02 15:14:55 +0530

    Fixed CORS Error

--- src/NotesService/NoteService.js
+++ src/NotesService/NoteService.js
@@ -5,14 +5,14 @@ export class NoteService {
 
     async getAllNotes() {
         try {
-            return await axios.get("/api/note");
+            return await axios.get(`${this.backend}/api/note`);
         } catch (error) {
             console.log("Error fetching all notes", error.message);
         }
     }
     async addNote({title, noteContent, bookmark}) {
         try {
-            return await axios.post("/api/note/add/", {
+            return await axios.post(`${this.backend}/api/note/add/`, {
                 title: title,
                 noteContent: noteContent || "",
                 bookmark: bookmark || false,
@@ -23,14 +23,14 @@ export class NoteService {
     }
     async updateNote(noteID, updatedNote) {
         try {
-            return await axios.put("api/note/update/" + noteID, updatedNote);
+            return await axios.put(`${this.backend}api/note/update/` + noteID, updatedNote);
         } catch (error) {
             console.log("Error updating note in DB", error.message);
         }
     }
     async deleteNote(noteID) {
         try {
-            return await axios.delete("api/note/delete/" + noteID);
+            return await axios.delete(`${this.backend}api/note/delete/` + noteID);
         } catch (error) {
             console.log("Error deleting note in DB", error.message);
         }
--- vite.config.js
+++ vite.config.js
@@ -2,24 +2,21 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     proxy: {
-//       "/api": "http://localhost:3000/"
+export default defineConfig({
+  plugins: [react()],
+  server: {
+  }
+})
+
+
+// export default ({mode}) => {
+//   process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+//   return defineConfig ({
+//     plugins: [react()],
+//     server: {
+//       proxy: {
+//         "/api": process.env.VITE_BACKEND_URI,
+//       },
 //     }
-//   }
-// })
-
-
-export default ({mode}) => {
-  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
-  return defineConfig ({
-    plugins: [react()],
-    server: {
-      proxy: {
-        "/api": process.env.VITE_BACKEND_URI,
-      },
-    }
-  })
-}
+//   })
+// }
