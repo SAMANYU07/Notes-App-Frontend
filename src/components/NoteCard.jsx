06c1e5296bd8338955c7dd47fd1fe0b0755501c7
SAMANYU07 <samanyu1003@gmail.com>
2024-09-04 13:59:29 +0530

    Added Transitions to Left Panel

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { updateCurrentNote } from '../features/NotesSlice';
-import { useTransition, animated } from 'react-spring';
 
 export default function NoteCard({
     title,
@@ -14,24 +13,16 @@ export default function NoteCard({
   const handleOnClick = async () => {
     dispatch(updateCurrentNote({title: title, noteContent: noteContent, bookmark: bookmark, _id: _id}))
   }
-  const cardTransition = useTransition(null, {
-    from: {opacity: 0},
-    enter: {opacity: 1},
-    leave: {opacity: 0},
-    config: {duration: 100},
-  });
   return (
     <>
-    {cardTransition((style, show) =>
-    <animated.div style={style} className={`h-[60px] flex flex-col border-t-2 hover:bg-[#ecedf0] transition-[0.2s] cursor-pointer ${_id === currentNote?._id ? "bg-[#ecedf0]" : ""}`} onClick={handleOnClick}>
+    <div className={`h-[60px] flex flex-col border-t-2 hover:bg-[#ecedf0] transition-[0.2s] cursor-pointer ${_id === currentNote?._id ? "bg-[#ecedf0]" : ""}`} onClick={handleOnClick}>
         <span className='font-bold'>{title?.length > 24 ? (title?.slice(0, 24) + "...") : title}</span>
         {noteContent?.length > 20 ?
         <p>{noteContent?.slice(0,24)}...</p>
         :
         <p>{noteContent}</p>
         }
-    </animated.div>
-    )}
+    </div>
     </>
   )
 }
--- src/components/NotesPanel.jsx
+++ src/components/NotesPanel.jsx
@@ -4,7 +4,7 @@ import NoteCard from './NoteCard';
 import { useDispatch, useSelector } from 'react-redux';
 import { addNote, toggleNewNoteWin, updateCurrentNote } from '../features/NotesSlice';
 import noteService from '../NotesService/NoteService';
-import { useTransition, animated } from 'react-spring';
+import { useTransition, animated, useTrail } from 'react-spring';
 
 export default function NotesPanel() {
   const [search, setSearch] = useState("");
@@ -20,6 +20,10 @@ export default function NotesPanel() {
     enter: {opacity: 1, transform: "scale(100%)"},
     leave: {opacity: 0, transform: "scale(90%)"},
   });
+  const cardsTrail = useTrail(notes?.length, {
+    from: {opacity: 0},
+    to: {opacity: 1},
+  });
   useEffect(() => {
     console.log("update: ", notes);
   }, [notes])
@@ -33,21 +37,29 @@ export default function NotesPanel() {
       <span className='font-bold text-[20px] mt-4 inline-block'>Notes App</span>
       <span className='mt-4'>{notes?.length} Notes</span>
       <div className='w-full mt-4 flex flex-col justify-center items-center'>
-        <input placeholder='Search' value={search} onChange={(event) => setSearch(event.target.value)} type="text" className='w-5/6 h-[40px]' />
+        <input placeholder='Search' value={search} onChange={(event) => setSearch(event.target.value)} type="text" className='w-5/6 h-[40px] outline-none' />
         <button onClick={handleAddNote} className='w-5/6 mt-4 bg-[#3f7afe] text-white rounded-lg addUpdateButton megaButton'>+ Add Note</button>
       </div>
       <div className='mt-8'>
         {
           search?.length > 0 ?
           notes?.map(note => {
-            if (note?.title.includes(search))
+            if (note?.title.toLowerCase().includes(search))
             return <NoteCard key={note._id} _id={note._id}
             title={note.title} noteContent={note.noteContent} bookmark={note.bookmark} />
           })
           :
-          notes?.map(note => <NoteCard key={note._id} _id={note._id}
-            title={note.title} noteContent={note.noteContent} bookmark={note.bookmark}
-            />)
+          cardsTrail?.map((style, index) =>
+          <animated.div style={style}>
+            <NoteCard
+            key={notes[index]?._id} _id={notes[index]?._id}
+            title={notes[index]?.title} noteContent={notes[index]?.noteContent} bookmark={notes[index]?.bookmark}
+            />
+          </animated.div>
+          )
+          // notes?.map(note => <NoteCard key={note._id} _id={note._id}
+          //   title={note.title} noteContent={note.noteContent} bookmark={note.bookmark}
+            // />)
           }
         <hr className='h-[2px] bg-[#e5e7eb]' />
       </div>
